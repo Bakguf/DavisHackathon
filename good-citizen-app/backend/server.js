feed09a4191cc3734cb76f14821425fd28cf8191
Bakguf <jeffreykim71@gmail.com>
2020-01-18 16:53:26 -0800

    Update server.js

--- good-citizen-app/backend/server.js
+++ good-citizen-app/backend/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.Atlas_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true});
+
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("MongoDB database connection established successfully");
+})
+
+const instancesRouter = require('./routes/instances');
+const usersRouter = require('./routes/users');
+
+
+
+app.use('/instances', instancesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log('Server is running on port: ${port}');
